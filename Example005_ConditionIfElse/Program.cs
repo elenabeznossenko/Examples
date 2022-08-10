c8a83821fbd5f5e2b908d580ffdc781321160e9c
elenabeznossenko <elena.beznossenko@gmail.com>
2022-08-10 23:16:41 +0500

    add condition if and else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Hooray,this is Masha");
+}
+else
+{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
